84b15a463e3a93690757d6dc89b1dd08c3bc10f2
Filipe Liuge Marimoto <41979826+FLMarimoto@users.noreply.github.com>
2018-10-14 22:11:26 -0300

    Update widgets.rb

--- db/dev_seeds/widgets.rb
+++ db/dev_seeds/widgets.rb
@@ -19,31 +19,11 @@ section "Creating header and cards for the homepage" do
   )
 
   Widget::Card.create!(
-    title: 'How do debates work?',
-    description: 'Anyone can open threads on any subject, creating separate spaces where people can discuss the proposed topic. Debates are valued by everybody, to highlight the most important issues.',
-    link_text: 'More about debates',
-    link_url: 'https://youtu.be/zU_0UN4VajY',
-    label: 'Debates',
-    header: FALSE,
-    image_attributes: create_image_attachment('debate')
-  )
-
-  Widget::Card.create!(
-    title: 'How do citizen proposals work?',
-    description: "A space for everyone to create a citizens' proposal and seek supports. Proposals which reach to enough supports will be voted and so, together we can decide the issues that matter to us.",
-    link_text: 'More about proposals',
-    link_url: 'https://youtu.be/ZHqBpT4uCoM',
-    label: 'Citizen proposals',
-    header: FALSE,
-    image_attributes: create_image_attachment('proposal')
-  )
-
-  Widget::Card.create!(
-    title: 'How do participatory budgets work?',
-    description: " Participatory budgets allow citizens to propose and decide directly how to spend part of the budget, with monitoring and rigorous evaluation of proposals by the institution. Maximum effectiveness and control with satisfaction for everyone.",
-    link_text: 'More about Participatory budgets',
+    title: 'Como orçamentos participativos funcionam?',
+    description: " Orçamentos participativos permitem que os cidadãos proponham e decidam diretamente como gastar parte do orçamento público, com monitoramento e avaliação rigorosos das propostas pelo governo. Máxima eficácia e controle para a satisfação de todos.",
+    link_text: 'Mais sobre orçamentos participativos',
     link_url: 'https://youtu.be/igQ8KGZdk9c',
-    label: 'Participatory budgets',
+    label: 'Orçamento participativo',
     header: FALSE,
     image_attributes: create_image_attachment('budget')
   )
